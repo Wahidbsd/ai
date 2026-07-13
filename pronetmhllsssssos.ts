bf77f4f1c23423167c637bf28ff6cadc8ca649b6
Wahidbsd <119776622+Wahidbsd@users.noreply.github.com>
2026-07-13 12:11:24 +0100

    Convert m3u8 playlist to TypeScript format

---
+++ pronetmhllsssssos.ts
@@ -0,0 +1,97 @@
+interface PlaylistEntry {
+  extinf: string;
+  tvgLogo?: string;
+  groupTitle?: string;
+  title: string;
+  url: string;
+}
+
+interface Playlist {
+  format: string;
+  entries: PlaylistEntry[];
+}
+
+const playlist: Playlist = {
+  format: "EXTM3U",
+  entries: [
+    // B. SPORT
+    {
+      extinf: "-1 group-title=\"B. SPORm3u8  F\"",
+      title: "---●★| BEIN SPORm3u8  F |★●---",
+      url: "http://pro.netmos.ovh:7355/live/MMTVO89S7DY7CNV7SD/P7HIDSPCN580WXH90/166014.m3u8"
+    },
+    {
+      extinf: "-1 tvg-logo=\"https://assem3u8 .bein.com/bmg/2022/11/NEWS_Channel_Full_Color.jpg\" group-title=\"B. SPORm3u8  F\"",
+      tvgLogo: "https://assem3u8 .bein.com/bmg/2022/11/NEWS_Channel_Full_Color.jpg",
+      groupTitle: "B. SPORm3u8  F",
+      title: "AR: Be News HD*",
+      url: "http://pro.netmos.ovh:7355/live/MMTVO89S7DY7CNV7SD/P7HIDSPCN580WXH90/166015.m3u8"
+    },
+    {
+      extinf: "-1 tvg-logo=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRItJ0fGBMirmU-rKoUyeakFCdYvQPuPrRGiQ&s\" group-title=\"B. SPORm3u8  F\"",
+      tvgLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRItJ0fGBMirmU-rKoUyeakFCdYvQPuPrRGiQ&s",
+      groupTitle: "B. SPORm3u8  F",
+      title: "BeIN SPORm3u8  GLOBAL",
+      url: "http://pro.netmos.ovh:7355/live/MMTVO89S7DY7CNV7SD/P7HIDSPCN580WXH90/231677.m3u8"
+    },
+    {
+      extinf: "-1 tvg-logo=\"http://pro.netmos.ovh:6051/img/beinsporm3u8 1ar.png\" group-title=\"B. SPORm3u8  F\"",
+      tvgLogo: "http://pro.netmos.ovh:6051/img/beinsporm3u8 1ar.png",
+      groupTitle: "B. SPORm3u8  F",
+      title: "AR: BeIN SPORm3u8  1 SD",
+      url: "http://pro.netmos.ovh:7355/live/MMTVO89S7DY7CNV7SD/P7HIDSPCN580WXH90/28242.m3u8"
+    },
+    {
+      extinf: "-1 tvg-logo=\"http://pro.netmos.ovh:6051/img/beinsporm3u8 2ar.png\" group-title=\"B. SPORm3u8  F\"",
+      tvgLogo: "http://pro.netmos.ovh:6051/img/beinsporm3u8 2ar.png",
+      groupTitle: "B. SPORm3u8  F",
+      title: "AR: BeIN SPORm3u8  2 SD",
+      url: "http://pro.netmos.ovh:7355/live/MMTVO89S7DY7CNV7SD/P7HIDSPCN580WXH90/28243.m3u8"
+    },
+    {
+      extinf: "-1 tvg-logo=\"http://pro.netmos.ovh:6051/img/beinsporm3u8 3ar.png\" group-title=\"B. SPORm3u8  F\"",
+      tvgLogo: "http://pro.netmos.ovh:6051/img/beinsporm3u8 3ar.png",
+      groupTitle: "B. SPORm3u8  F",
+      title: "AR: BeIN SPORm3u8  3 SD",
+      url: "http://pro.netmos.ovh:7355/live/MMTVO89S7DY7CNV7SD/P7HIDSPCN580WXH90/28244.m3u8"
+    },
+    {
+      extinf: "-1 tvg-logo=\"http://pro.netmos.ovh:6051/img/beinsporm3u8 4ar.png\" group-title=\"B. SPORm3u8  F\"",
+      tvgLogo: "http://pro.netmos.ovh:6051/img/beinsporm3u8 4ar.png",
+      groupTitle: "B. SPORm3u8  F",
+      title: "AR: BeIN SPORm3u8  4 SD",
+      url: "http://pro.netmos.ovh:7355/live/MMTVO89S7DY7CNV7SD/P7HIDSPCN580WXH90/28245.m3u8"
+    },
+    {
+      extinf: "-1 tvg-logo=\"http://pro.netmos.ovh:6051/img/beinsporm3u8 5ar.png\" group-title=\"B. SPORm3u8  F\"",
+      tvgLogo: "http://pro.netmos.ovh:6051/img/beinsporm3u8 5ar.png",
+      groupTitle: "B. SPORm3u8  F",
+      title: "AR: BeIN SPORm3u8  5 SD",
+      url: "http://pro.netmos.ovh:7355/live/MMTVO89S7DY7CNV7SD/P7HIDSPCN580WXH90/28246.m3u8"
+    },
+    {
+      extinf: "-1 tvg-logo=\"http://pro.netmos.ovh:6051/img/beinsporm3u8 6ar.png\" group-title=\"B. SPORm3u8  F\"",
+      tvgLogo: "http://pro.netmos.ovh:6051/img/beinsporm3u8 6ar.png",
+      groupTitle: "B. SPORm3u8  F",
+      title: "AR: BeIN SPORm3u8  6 SD",
+      url: "http://pro.netmos.ovh:7355/live/MMTVO89S7DY7CNV7SD/P7HIDSPCN580WXH90/28247.m3u8"
+    },
+    {
+      extinf: "-1 tvg-logo=\"http://pro.netmos.ovh:6051/img/beinsporm3u8 7ar.png\" group-title=\"B. SPORm3u8  F\"",
+      tvgLogo: "http://pro.netmos.ovh:6051/img/beinsporm3u8 7ar.png",
+      groupTitle: "B. SPORm3u8  F",
+      title: "AR: BeIN SPORm3u8  7 SD",
+      url: "http://pro.netmos.ovh:7355/live/MMTVO89S7DY7CNV7SD/P7HIDSPCN580WXH90/28248.m3u8"
+    },
+    {
+      extinf: "-1 tvg-logo=\"http://pro.netmos.ovh:6051/img/beinsporm3u8 8ar.png\" group-title=\"B. SPORm3u8  F\"",
+      tvgLogo: "http://pro.netmos.ovh:6051/img/beinsporm3u8 8ar.png",
+      groupTitle: "B. SPORm3u8  F",
+      title: "AR: BeIN SPORm3u8  8 SD",
+      url: "http://pro.netmos.ovh:7355/live/MMTVO89S7DY7CNV7SD/P7HIDSPCN580WXH90/78792.m3u8"
+    },
+    // Add more entries as needed...
+  ]
+};
+
+export default playlist;
